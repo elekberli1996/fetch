aa1be11ee46a1ad290002c24f5ebf9ca16684e2a
emin elekberli <elekberli1996@gmail.com>
2024-04-04 12:11:55 +0400

    commit

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-let number=1;
-fetch("https://jsonfakery.com/movies/paginated").then(res=>
-res.json().then(movies => movies.data.map(movie=>
-    {console.log(`${number} ci film ismi: ${movie.original_title}`)
-    number++;}
-)))
+// 1ci varyant
+
+
+// let number=1;
+// fetch("https://jsonfakery.com/movies/paginated").then(res=>
+// res.json().then(movies => movies.data.map(movie=>
+//     {console.log(`${number} ci film ismi: ${movie.original_title}`)
+//     number++;}
+// )))
+
+
+//2ci varyant
+// const options = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2IwYWNhNzBkMGIxOTIzODM4NTVlMGQyNDE1ZDMxZCIsInN1YiI6IjY2MGU1MGI0ZDQwMGYzMDE3ZDA3MjViMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NHnNO0lMqAuijbsd8U75DsO5Hx4kE5Uk0RYGwq2lmjo'
+//     }
+//   };
+
+
+// fetch('https://api.themoviedb.org/3/account/21183685//watchlist/movies?language=en-US&page=1&sort_by=created_at.asc', options).then(res=>
+// res.json().then(movies =>{
+//     movies.results.map(m=>console.log(m.original_title)) 
+// }
+// ))
 
